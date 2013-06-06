cf5f1595093b20f208ce422aa1e804a4434862de
StefLB <stefanie.lehmann-berlin@web.de>
2013-06-06 23:40:09 +0200

    Libavg Bildschirm
    sieht kacke aus

---
+++ ScreenApp/src/screen/main.py
@@ -0,0 +1,43 @@
+'''
+Created on 06.06.2013
+
+@author: Steffi
+'''
+
+from libavg import *
+
+class screen(AVGApp):
+    def __init__(self, parentNode):
+        
+        player = avg.Player.get()  
+        global a,b,z
+        (a,b) = parentNode.size
+        canvas = player.createMainCanvas(size=(a,b)) #Aufloesung Bildschirm
+        self.rootNode = canvas.getRootNode()
+        self.back = avg.RectNode (pos=(0,0), size=(a,b), parent=self.rootNode, color="A4A4A4", fillcolor="A4A4A4", fillopacity=1) 
+        self.z = int (a-449)
+        self.title=avg.WordsNode (font="arial", variant="Bold", text="DjCrowd - Canossa", color="000000", fontsize=40, alignment="left", parent=self.rootNode)
+        print self.z
+        self.timer=avg.WordsNode (font="arial", variant="Bold", text="Song-Countdown=00:00", color="000000", fontsize=40, indent=self.z, parent=self.rootNode)
+
+        
+        def left():
+            self.divNode=avg.DivNode(pos=(0,50), size=((a/2)-20,b-50),parent=self.rootNode)
+            self.left=avg.RectNode (pos=(0,0), size=((a/2)-20, b-50), parent=self.divNode, color="F0F0F0", fillopacity=1)
+            self.title=avg.WordsNode (font="arial", variant="Bold", text="Top 7 Songs", color="000000", fontsize=30, parent=self.divNode, alignment="center")
+            
+        def right():
+            self.divNode=avg.DivNode(pos=(a/2,50), size=((a/2)-20,b-50),parent=self.rootNode)
+            self.left=avg.RectNode (pos=(0,0), size=((a/2)-20, b-50), parent=self.divNode, color="F0F0F4", fillopacity=1)
+        
+        left()
+        right()
+        
+        
+        
+  
+        
+if __name__=='__main__':
+    screen.start(resolution=(1024, 768))   
+  
+
