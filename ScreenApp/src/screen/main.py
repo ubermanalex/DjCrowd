bebfe3c8f560d2e1cd7c515385bcddf55fc17e4f
StefLB <stefanie.lehmann-berlin@web.de>
2013-06-12 12:47:40 +0200

    Countdown hinzugefügt

--- ScreenApp/src/screen/main.py
+++ ScreenApp/src/screen/main.py
@@ -5,6 +5,8 @@ Created on 06.06.2013
 '''
 
 from libavg import *
+import time
+import thread
 
 class screen(AVGApp):
     def __init__(self, parentNode):
@@ -18,7 +20,7 @@ class screen(AVGApp):
         self.z = int (a-449)
         self.title=avg.WordsNode (font="arial", variant="Bold", text="DjCrowd - Canossa", color="000000", fontsize=40, alignment="left", parent=self.rootNode)
         print self.z
-        self.timer=avg.WordsNode (font="arial", variant="Bold", text="Song-Countdown=00:00", color="000000", fontsize=40, indent=self.z, parent=self.rootNode)
+        self.timer=avg.WordsNode (font="arial", variant="Bold", text="Song-Countdown 30:00", color="000000", fontsize=40, indent=self.z, parent=self.rootNode)
 
         
         def left():
@@ -29,9 +31,32 @@ class screen(AVGApp):
         def right():
             self.divNode=avg.DivNode(pos=(a/2,50), size=((a/2)-20,b-50),parent=self.rootNode)
             self.left=avg.RectNode (pos=(0,0), size=((a/2)-20, b-50), parent=self.divNode, color="F0F0F4", fillopacity=1)
+            
+        def countdown(m,s):
+            
+            def MsToSecs(m,s):
+                return m*60 + s
+
+            def secsToMs(secs):
+                mins = secs//60
+                secs -= mins*60
+                mins = str(mins)
+                secs = str(secs)
+                return mins,secs
+            
+            seconds = MsToSecs(m,s)
+            while seconds > 0:
+                (mint,sect)=secsToMs(seconds)
+                self.timer.text="Countdown " + mint + ":" + sect
+                seconds -= 1
+                time.sleep(1)
+                if seconds ==0:
+                    seconds = 1800
         
+            
         left()
         right()
+        thread.start_new_thread(countdown,(0,2))
         
         
         
---
+++ Your Song/public_html/media.js
@@ -0,0 +1,63 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function Song (interprets,titels,points) { /* Objekt Song mit seinen Methoden*/
+	this.interpret = interprets;
+	this.titel = titels;
+        this.points = points;
+        
+        this.toStrI= function(){
+        return "Interpret: "+this.interpret+" ; Song: "+this.titel+" ; Punkte: "+this.points;
+        };
+        
+        this.toStrS = function(){
+            return "Interpret: "+this.interpret+" ; Song: "+this.titel+" ; Punkte: "+this.points;
+        };      
+	}
+
+function SongDatabase() {
+    
+    this.database =new Array();
+
+    this.addSong = function(interpreta, titlea, pointsa) {
+	newsong=new Song(interpreta, titlea, pointsa);
+        this.database.push(newsong);
+	};
+        
+    this.getSongsByInterpret= function (interpret) {
+        var result = '';
+        for (var i=0; i<this.database.length; ++i){
+            if (this.database[i].interpret === interpret) {
+                result = result + this.database[i].toStrI() + '\n';
+            }
+        var res = result;
+        document.getElementById("ergebnisse").value = res;
+		
+    }};
+    
+      this.getSongsBySong= function (titel) {
+        var result = '';
+        for (var i=0; i<this.database.length; ++i){
+            if (this.database[i].titel === titel) {
+                result = result + this.database[i].toStrS() + '\n';
+            }
+        }
+        var res = result;
+        document.getElementById("ergebnisse").value = res;
+    };
+    
+          this.AddVote= function (song,vote) {
+        for (var i=0; i<this.database.length; ++i){
+            if (this.database[i].song === song) {
+                result = this.database[i];
+                result.points+=vote;
+                return result;
+            }
+        var res = result;
+		
+    }};
+    
+ 
+}
+
